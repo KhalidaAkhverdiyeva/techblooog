96c711f1df16fd1a13ddc61d24e384487e839904
Khalida <xalide.haqverdiyeva@gmail.com>
2024-08-08 13:38:10 +0400

    fix: resolve image resizing bug

---
+++ src/components/PicksCard.component.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const PicksCard = ({ imageSrc, buttonText, number, title, buttonColor }) => {
+    return (
+      <div className='relative max-w-sm mx-auto rounded-lg overflow-hidden w-[25%]'>
+        <div className='relative'>
+          <img 
+            src={imageSrc} 
+            alt='Card' 
+            className='w-full h-[190px] object-cover rounded-[10px]'
+          />
+          <button 
+            className='absolute top-0 left-0 font-semibold text-white text-[12px] px-2 py-1 rounded'
+            style={{ backgroundColor: buttonColor }}
+          >
+            {buttonText}
+          </button>
+        </div>
+  
+        <div className='flex py-4'>
+          <div className='text-[#CCCCCC] text-4xl font-[600] mr-4'>{number}</div>
+          <div>
+            <p className='text-[#161616] mb-2 text-[18px] font-bold'>
+              {title}
+            </p>
+            <div className='flex gap-3 items-center'>
+              <p className='text-[#191919] font-[600] text-[12px]'>Shane Doe</p>
+              <div className='text-gray-400 font-[600] text-[12px]'>Mar 15, 2020</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+export default PicksCard
--- src/pages/Home.page.jsx
+++ src/pages/Home.page.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import HeroCard from '../components/HeroCard.component'
 import HeroCardSmall from '../components/HeroCardSmall'
 import RoundedCard from '../components/RoundedCard.component'
+import PicksCard from '../components/PicksCard.component'
 
 const Home = () => {
   return (
+    <>
     <div className='flex gap-[40px] my-[40px]'>
-      <div className='w-[70%]'>
+      <div id='hero part' className='w-[70%]'>
         <HeroCard/>
         <div className='flex gap-9'>
           <HeroCardSmall 
@@ -18,7 +20,7 @@ const Home = () => {
         </div>
         
       </div>
-      <div className='w-[30%] overflow-hidden'>
+      <div id="people's fav" className='w-[30%] overflow-hidden'>
         <h2 className='text-[19px] text-[#161616] font-bold mb-[10px]'>People's Favorite</h2>
         <div>
           <RoundedCard
@@ -56,8 +58,58 @@ const Home = () => {
 
 
       </div>
-     
     </div>
+
+    <div id='editors-pick' className=''>
+      <div className='flex items-center mb-[30px]'> 
+        <div className='flex flex-col flex-1 items-center gap-[4px]'>
+          <div className='bg-gray-300 h-[1px] w-full '></div>
+          <div className='bg-gray-300 h-[1px] w-full'></div>
+        </div>
+
+        <div className='mx-4 text-center text-[28px] font-bold text-[#161616]'>Editor's Picks</div>
+
+        <div className='flex flex-col flex-1 items-center gap-[4px]'>
+          <div className='bg-gray-300 h-[1px] w-full  '></div>
+          <div className='bg-gray-300 h-[1px] w-full'></div>
+        </div>
+      </div>
+      <div id='pickscards' className='flex gap-[30px]'>
+      <PicksCard 
+        imageSrc='https://smartmag.theme-sphere.com/tech-blog/wp-content/uploads/sites/35/2022/11/akhil-yerabati-Q2uV5TkjNz8-unsplash-450x255.jpg'
+        buttonText='GADGETS'
+        number='1'
+        title='Study: Earbuds Use, Youngsters at High Risk of Hearing Loss'
+        buttonColor='#B400B4'
+      />
+      <PicksCard 
+        imageSrc='https://smartmag.theme-sphere.com/tech-blog/wp-content/uploads/sites/35/2022/11/greg-rakozy-oMpAz-DN-9I-unsplash-450x300.jpg'
+        buttonText='TECHNOLOGY'
+        number='2'
+        title='Telescope is Revealing the Galaxies of the Universe Like Never Before'
+        buttonColor='#3C3FDE'
+      />
+      <PicksCard 
+        imageSrc='https://smartmag.theme-sphere.com/tech-blog/wp-content/uploads/sites/35/2022/11/Depositphotos_429887216_XL-1-450x289.jpg'
+        buttonText='PHONES'
+        number='3'
+        title='CarPlay Concept Shows Off a Modular UI Inspired by Next-Gen Design'
+        buttonColor='#00A0A0'
+      />
+      <PicksCard 
+        imageSrc='https://smartmag.theme-sphere.com/tech-blog/wp-content/uploads/sites/35/2022/11/sunrise-king-NK-cB-l1cv0-unsplash-1-450x271.jpg'
+        buttonText='TECHNOLOGY'
+        number='4'
+        title='Latest Windows 11 Preview Build Finally Lets You Search for Copied Text'
+        buttonColor='#3C3FDE'
+      />
+
+      </div>
+      
+    </div>
+    
+    </>
+    
   )
 }
 
